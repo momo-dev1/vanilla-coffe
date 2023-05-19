ff0de5f7f2ddc0bd5e4da08e366c7a020940edab
momo-dev1 <didy1234567@gmail.com>
2023-05-19 20:15:12 +0300

    menu page

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,11 +1,28 @@
 import API from '../services/API.js';
-import ProductItem  from './ProductItem.js';
+import ProductItem from './ProductItem.js';
 
 export default class MenuPage extends HTMLElement {
     constructor() {
         super();
+        this.root = this.attachShadow({ mode: 'open' })
+
+        const styles = document.createElement("style")
+        this.root.appendChild(styles)
+        async function loadCss() {
+            const req = await fetch("/components/MenuPage.css")
+            const css = await req.text()
+            styles.textContent = css
+        }
+        loadCss()
     }
 
+
+    // method is called automatically by the browser when the element is inserted into the DOM
+    connectedCallback() {
+        const template = document.getElementById("menu-page-template")
+        const content = template.content.cloneNode(true)
+        this.root.appendChild(content)
+    }
 }
 
 customElements.define("menu-page", MenuPage);
