734b091a4d9556b99b9835bc183c90e88e900b88
momo-dev1 <didy1234567@gmail.com>
2023-05-20 12:26:35 +0300

    making proxiedStore

--- services/Store.js
+++ services/Store.js
@@ -3,8 +3,19 @@ import API from './API.js'
 const Store = {
     menu: null,
     cart: []
-} 
+}
 
-const proxiedStore = new Proxy(Store, {})
+const proxiedStore = new Proxy(Store, {
+    set(target, property, value) {
+        target[property] = value
+        if (property === "menu") {
+            window.dispatchEvent(new Event("appmenuchanged"))
+        }
+        if (property === "cart") {
+            window.dispatchEvent(new Event("appcartchanged"))
+        }
+        return true
+    }
+})
 
 export default proxiedStore;
