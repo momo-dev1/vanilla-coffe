ccc23991bea640221a4c2f6764c39fd8fb9a638f
momo-dev1 <didy1234567@gmail.com>
2023-05-21 21:25:34 +0300

    render menu

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -17,11 +17,40 @@ export default class MenuPage extends HTMLElement {
     }
 
 
-    // method is called automatically by the browser when the element is inserted into the DOM
+    /* 
+    method is called automatically by the browser when
+    the element is inserted into the DOM
+    */
     connectedCallback() {
         const template = document.getElementById("menu-page-template")
         const content = template.content.cloneNode(true)
         this.root.appendChild(content)
+        window.addEventListener("appmenuchanged", () => {
+            this.render()
+        })
+    }
+
+    render() {
+        if (app.store.menu) {
+            this.root.getElementById("menu").innerHTML = ""
+            for (let category of app.store.menu) {
+                const liCategory = document.createElement("li")
+                liCategory.innerHTML = `
+                <h3>${ category.name }</h3>
+                <ul class="category"></ul>
+                `
+                this.root.getElementById("menu").appendChild(liCategory)
+
+                category.products.forEach((product) => {
+                    const item = document.createElement("product-item")
+                    item.dataset.product = JSON.stringify(product)
+                    liCategory.querySelector("ul").appendChild(item)
+                })
+            }
+
+        } else {
+            this.root.getElementById("menu").innerHTML = "Loading..."
+        }
     }
 }
 
